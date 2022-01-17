a7f5f8fa6636e7d5c04bf7799ea80e38d87b8edd
giandimi <56236215+giandimi@users.noreply.github.com>
2022-01-17 17:21:58 +0200

    Add files via upload

--- BestFit.java
+++ BestFit.java
@@ -1,19 +1,53 @@
 import java.util.ArrayList;
 
 public class BestFit extends MemoryAllocationAlgorithm {
-    
+
     public BestFit(int[] availableBlockSizes) {
         super(availableBlockSizes);
     }
 
-    public int fitProcess(Process p, ArrayList<MemorySlot> currentlyUsedMemorySlots) {
-        boolean fit = false;
-        int address = -1;
-        /* TODO: you need to add some code here
-         * Hint: this should return the memory address where the process was
-         * loaded into if the process fits. In case the process doesn't fit, it
-         * should return -1. */
 
+    /**
+     * This method is used to load a process into a memory slot using the Best Fit algorithm
+     * and returns the address of the memory slot, if the process fits.
+     * @param p is a Process object that needs tobe loaded into memory.
+     * @param currentlyUsedMemorySlots is the Arraylist containing the Memory slots that are occupied by one or more processes.
+     * @return the address of the memory slot where the process is loaded or -1 if the process doesn't fit anywhere.
+     */
+    public int fitProcess(Process p, ArrayList<MemorySlot> currentlyUsedMemorySlots) {
+        boolean fit = false; // Turns true if the process fits in memory.
+
+        int address = -1; //the address of the memory slot where the process is loaded to.
+
+        int requirementMemory = p.getMemoryRequirements(); //The requirement memory that the process needs.
+
+        int bestFit = 999999; //Keeps the size of the smallest memory slot that the process fits.
+
+        /*
+        Loop that searches the smallest space
+        in the availableBlockSizes Array and the currentlyUsedMemorySlots,
+        that the process fits.
+         */
+        for(int i=0; i<availableBlockSizes.length; i++){
+            if(requirementMemory < availableBlockSizes[i]){ //If the process fits.
+                if(currentlyUsedMemorySlots.get(i) != null){ //If the block is used by another process.
+                    int freeSpace = currentlyUsedMemorySlots.get(i).getBlockEnd() - currentlyUsedMemorySlots.get(i).getEnd(); //The free space in the block.
+                    if( (requirementMemory < freeSpace) && (freeSpace < bestFit) ){ //If the process fits and the space is the smallest so far.
+                        bestFit = freeSpace; //Then this space is best for the process to fit.
+                        address = i; //Address takes the value of the current address.
+                    }
+                }
+                else{ //If the block is empty.
+                    if(availableBlockSizes[i] < bestFit){ //If the space is the smallest so far.
+                        bestFit = availableBlockSizes[i]; //Then this space is best for the process to fit.
+                        address = i; //Address takes the value of the current address.
+                    }
+                }
+            }
+        }
+        if(address != -1){ //If the process does fit in memory
+            fit = true;
+        }
         return address;
     }
 
--- MMU.java
+++ MMU.java
@@ -5,19 +5,55 @@ public class MMU {
     private final int[] availableBlockSizes;
     private MemoryAllocationAlgorithm algorithm;
     private ArrayList<MemorySlot> currentlyUsedMemorySlots;
-    
+
+
     public MMU(int[] availableBlockSizes, MemoryAllocationAlgorithm algorithm) {
         this.availableBlockSizes = availableBlockSizes;
         this.algorithm = algorithm;
         this.currentlyUsedMemorySlots = new ArrayList<MemorySlot>();
     }
 
+    /**
+     * loadProcessIntoRam is a boolean method that loads a process into memory, so that it can be executed by CPU
+     * and returns it was successful or not.
+     * @param p is a Process object that needs tobe loaded into memory.
+     * @return true if the process is loaded into memory and false if not.
+     */
     public boolean loadProcessIntoRAM(Process p) {
-        boolean fit = false;
-        /* TODO: you need to add some code here
-         * Hint: this should return true if the process was able to fit into memory
-         * and false if not */
-        
+        boolean fit = false; // Turns true if the process fits in memory.
+
+        /* Integer variables to set the arguments of the MemorySlot object,
+        * where process is going to be loaded. */
+        int start;
+        int end;
+        int blockStart;
+        int blockEnd;
+
+        /* Call of the algorithm's function that loads the process into memory.
+        * Returns the address of the memory, where the process is loaded. */
+        int address = algorithm.fitProcess(p,currentlyUsedMemorySlots);
+
+        if(address != -1 ){     //If the process does fit in memory
+
+            /* If there is already a process in this address,the current process is being added at the end of the block. */
+            if (currentlyUsedMemorySlots.get(address) != null){
+                start = currentlyUsedMemorySlots.get(address).getStart();
+                end = currentlyUsedMemorySlots.get(address).getEnd() + p.getMemoryRequirements();
+                blockStart = currentlyUsedMemorySlots.get(address).getBlockStart();
+                blockEnd = currentlyUsedMemorySlots.get(address).getBlockEnd();
+            }
+            /* Else the current process is being added at the beginning of the block. */
+            else{
+                start = 0;
+                end = p.getMemoryRequirements(); //The end = start + the requirement memory that the process needs.
+                blockStart = 0;
+                blockEnd = availableBlockSizes[address]; //The blockEnd = blockStart + the available space that this memory cell has.
+            }
+            MemorySlot slot = new MemorySlot(start,end,blockStart,blockEnd); //The memory slot, where the process is being loaded.
+            currentlyUsedMemorySlots.add(address,slot); //Add the memory slot in the Arraylist
+            fit = true; //The process fits in memory, so the variable turns true.
+        }
+
         return fit;
     }
 }
