dc80523f3e727cac87824642a48ca85a457e2d7d
gchatzief <gchatzief@csd.auth.gr>
2022-01-25 22:26:28 +0200

    7

--- CPU.java
+++ CPU.java
@@ -16,17 +16,19 @@ public class  CPU {
         this.processes = processes;
         processesNew=new ArrayList<>();
     }
-    int i;
+    int i=0;
+
     public void run() {
         /* TODO: you need to add some code here
          * Hint: you need to run tick() in a loop, until there is nothing else to do... */
+
          do
          {
              tick();
              i++;
 
          }
-         while (flag==false && i<processes.length);
+         while (i<(processes.length));
 
 
     }
@@ -34,40 +36,29 @@ public class  CPU {
     public void tick() {
         /* TODO: you need to add some code here
          * Hint: this method should run once for every CPU cycle */
-
-        if(mmu.loadProcessIntoRAM(processes[i]))
-        {
-            scheduler.addProcess(processes[i]);
-            processes[i].getPCB().setState(ProcessState.READY,clock);
-            System.out.println("1"+i);
-        }
-        else if (scheduler.getNextProcess().getPCB().getState()==ProcessState.RUNNING)
-        {
-            scheduler.getNextProcess().getPCB().setState(ProcessState.TERMINATED,clock);
-            scheduler.removeProcess(scheduler.getNextProcess());
-            scheduler.addProcess(processesNew.get(0));
-            processesNew.remove(0);
-            System.out.println("3"+i);
-        }
-        else if (mmu.loadProcessIntoRAM(processes[i])==false)
-        {
-            processesNew.add(processes[i]);
-            System.out.println("2"+i);
-            return;
-
-        }
-        else if (scheduler.processes.size()!=0)
-        {
-            scheduler.getNextProcess().getPCB().setState(ProcessState.RUNNING,clock);
-            System.out.println("4"+i);
-
-        }
-        else
-        {
-            flag =true;
-        }
+       if(processes[i].getPCB().getState()==ProcessState.NEW)
+       {
+           if (mmu.loadProcessIntoRAM(processes[i]))
+           {
+               scheduler.addProcess(processes[i]);
+               processes[i].waitInBackground();
 
 
+               i--;
+
+
+
+               System.out.println(i);
+           }
+       }
+       else
+       {
+           scheduler.getNextProcess();
+           processes[i].run();
+           processes[i].getPCB().setState(ProcessState.TERMINATED,clock);
+           scheduler.removeProcess(processes[i]);
+           mmu.remove(processes[i]);
+       }
 
 
 
