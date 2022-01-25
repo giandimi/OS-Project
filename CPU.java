474deba3f5ea8f0ecb5ab40c432b582ae9902abb
gchatzief <gchatzief@csd.auth.gr>
2022-01-25 23:43:14 +0200

    8

--- CPU.java
+++ CPU.java
@@ -8,29 +8,25 @@ public class  CPU {
     private MMU mmu;
     private Process[] processes;
     private int currentProcess;
-    private ArrayList<Process>processesNew;
+
     boolean flag = false;
     public CPU(Scheduler scheduler, MMU mmu, Process[] processes) {
         this.scheduler = scheduler;
         this.mmu = mmu;
         this.processes = processes;
-        processesNew=new ArrayList<>();
     }
     int i=0;
 
     public void run() {
         /* TODO: you need to add some code here
          * Hint: you need to run tick() in a loop, until there is nothing else to do... */
+             do
+             {
+                 tick();
+                 i++;
 
-         do
-         {
-             tick();
-             i++;
-
-         }
-         while (i<(processes.length));
-
-
+             }
+             while (i<(processes.length));
     }
 
     public void tick() {
@@ -42,19 +38,15 @@ public class  CPU {
            {
                scheduler.addProcess(processes[i]);
                processes[i].waitInBackground();
-
-
                i--;
-
-
-
-               System.out.println(i);
            }
        }
        else
        {
            scheduler.getNextProcess();
            processes[i].run();
+           processes[i].getWaitingTime();
+           processes[i].getResponseTime();
            processes[i].getPCB().setState(ProcessState.TERMINATED,clock);
            scheduler.removeProcess(processes[i]);
            mmu.remove(processes[i]);
